c9971fc5c4a1471edc5c79810c58f50cde0b9fc6
neribalbuena <nerybalbuena293@gmail.com>
2026-08-06 16:39:54 -0300

    Refactor Filters component to improve category selection

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -3,14 +3,16 @@ export const Filters = ({ categories, selectedCategory, onSelectCategory }) => {
     <div className="filters-container">
       <span className="filters-label">Categorías:</span>
       <button
+        //si la categoría seleccionada = todas le agrega active 
         className={`filter-btn ${selectedCategory === 'Todas' ? 'active' : ''}`}
         onClick={() => onSelectCategory('Todas')}
       >
         Todas
       </button>
-
+      
       {categories.map((category) => (
         <button
+          //Recorre el array categories con map y genera un botón por cada un
           key={category}
           className={`filter-btn ${selectedCategory === category ? 'active' : ''}`}
           onClick={() => onSelectCategory(category)}
